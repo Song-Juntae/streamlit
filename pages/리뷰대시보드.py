aa3b2c27eef54d6b3c2f527521cd09547a89a5ac
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-05 17:36:06 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -110,7 +110,7 @@ with col1_4:
     type(end_date) == type(start_date[0])
     type(df_리뷰_감성분석결과['time'][0]) == type(pd.Timestamp(start_date[0]))
     type(df_리뷰_감성분석결과['time'][0]) == type(pd.to_datetime(end_date))
-    기간마스크 = ((df_리뷰_감성분석결과['time'] >= pd.to_datetime(start_date[0]) & df_리뷰_감성분석결과['time'] <= pd.to_datetime(end_date)))
+    기간마스크 = ((df_리뷰_감성분석결과['time'] >= pd.to_datetime(start_date[0])) & (df_리뷰_감성분석결과['time'] <= pd.to_datetime(end_date)))
 
 with col3_1:
     추가불용어 = st.text_input('불용어를 추가하세요', '')
