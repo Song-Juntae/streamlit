632275d20dd4f0b4556b7e994ddc85629599f6c9
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-05 17:43:14 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -129,15 +129,14 @@ if 추가불용어.find(',') == -1:
 
 with col3_3:
     키워드 = st.text_input('키워드를 입력해주세요', '제라늄')
-    try:
-        if 키워드.find(',') == -1:
-            st.write('예시 : 뿌리, 제라늄, 식물, 응애')
-            키워드 = [키워드]
-        elif 키워드.find(',') != -1:
-            st.write('설정된 키워드: ', 키워드)
-            키워드 = [i.strip() for i in 키워드.split(',')]
-    except:
-        st.write('존재하지 않는 키워드예요.')
+    if 키워드.find(',') == -1:
+        st.write('예시 : 뿌리, 제라늄, 식물, 응애')
+        키워드 = [키워드]
+    elif 키워드.find(',') != -1:
+        st.write('설정된 키워드: ', 키워드)
+        키워드 = [i.strip() for i in 키워드.split(',')]
+    else:
+        st.write('문제가 생겼어요.')
      
 ########################################################################################################################
 def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=None, sentiment = None, item = None, source = None , 품사='noun'):
@@ -230,78 +229,86 @@ with col4_3:
 
 reviews = [eval(i) for i in df_리뷰_감성분석결과[기간마스크 & 회사종류마스크][품사]]
 
-networks = []
-for review in reviews:
-    network_review = [w for w in review if len(w) > 1]
-    networks.append(network_review)
+def 네트워크(reviews):
+    networks = []
+    for review in reviews:
+        network_review = [w for w in review if len(w) > 1]
+        networks.append(network_review)
 
-model = Word2Vec(networks, vector_size=100, window=5, min_count=1, workers=4, epochs=100)
+    model = Word2Vec(networks, vector_size=100, window=5, min_count=1, workers=4, epochs=100)
 
-G = nx.Graph(font_path='/app/streamlit/font/NanumBarunGothic.ttf')
+    G = nx.Graph(font_path='/app/streamlit/font/NanumBarunGothic.ttf')
 
-# 중심 노드들을 노드로 추가
-for keyword in 키워드:
-    G.add_node(keyword)
-    # 주어진 키워드와 가장 유사한 20개의 단어 추출
-    similar_words = model.wv.most_similar(keyword, topn=20)
-    # 유사한 단어들을 노드로 추가하고, 주어진 키워드와의 연결선 추가
-    for word, score in similar_words:
-        G.add_node(word)
-        G.add_edge(keyword, word, weight=score)
-        
-# 노드 크기 결정
-size_dict = nx.degree_centrality(G)
+    # 중심 노드들을 노드로 추가
+    for keyword in 키워드:
+        G.add_node(keyword)
+        # 주어진 키워드와 가장 유사한 20개의 단어 추출
+        similar_words = model.wv.most_similar(keyword, topn=20)
+        # 유사한 단어들을 노드로 추가하고, 주어진 키워드와의 연결선 추가
+        for word, score in similar_words:
+            G.add_node(word)
+            G.add_edge(keyword, word, weight=score)
+            
+    # 노드 크기 결정
+    size_dict = nx.degree_centrality(G)
 
-# 노드 크기 설정
-node_size = []
-for node in G.nodes():
-    if node in 키워드:
-        node_size.append(5000)
-    else:
-        node_size.append(1000)
+    # 노드 크기 설정
+    node_size = []
+    for node in G.nodes():
+        if node in 키워드:
+            node_size.append(5000)
+        else:
+            node_size.append(1000)
 
-# 클러스터링
-clusters = list(nx.algorithms.community.greedy_modularity_communities(G))
-cluster_labels = {}
-for i, cluster in enumerate(clusters):
-    for node in cluster:
-        cluster_labels[node] = i
-        
-# 노드 색상 결정
-color_palette = ["#f39c9c", "#f7b977", "#fff4c4", "#d8f4b9", "#9ed6b5", "#9ce8f4", "#a1a4f4", "#e4b8f9", "#f4a2e6", "#c2c2c2"]
-node_colors = [color_palette[cluster_labels[node] % len(color_palette)] for node in G.nodes()]
+    # 클러스터링
+    clusters = list(nx.algorithms.community.greedy_modularity_communities(G))
+    cluster_labels = {}
+    for i, cluster in enumerate(clusters):
+        for node in cluster:
+            cluster_labels[node] = i
+            
+    # 노드 색상 결정
+    color_palette = ["#f39c9c", "#f7b977", "#fff4c4", "#d8f4b9", "#9ed6b5", "#9ce8f4", "#a1a4f4", "#e4b8f9", "#f4a2e6", "#c2c2c2"]
+    node_colors = [color_palette[cluster_labels[node] % len(color_palette)] for node in G.nodes()]
 
-# 노드에 라벨과 연결 강도 값 추가
-edge_weights = [d['weight'] for u, v, d in G.edges(data=True)]
+    # 노드에 라벨과 연결 강도 값 추가
+    edge_weights = [d['weight'] for u, v, d in G.edges(data=True)]
 
-# 선의 길이를 변경 pos
-plt.figure(figsize=(15,15))
-pos = nx.spring_layout(G, seed=42, k=0.15)
-nx.draw(G, pos, font_family='NanumGothic', with_labels=True, node_size=node_size, node_color=node_colors, alpha=0.8, linewidths=1,
-        font_size=9, font_color="black", font_weight="medium", edge_color="grey", width=edge_weights)
-
-
-# 중심 노드들끼리 겹치는 단어 출력
-overlapping_키워드 = set()
-for i, keyword1 in enumerate(키워드):
-    for j, keyword2 in enumerate(키워드):
-        if i < j and keyword1 in G and keyword2 in G:
-            if nx.has_path(G, keyword1, keyword2):
-                overlapping_키워드.add(keyword1)
-                overlapping_키워드.add(keyword2)
-if overlapping_키워드:
-    print(f"다음 중심 키워드들끼리 연관성이 있어 중복될 가능성이 있습니다: {', '.join(overlapping_키워드)}")
+    # 선의 길이를 변경 pos
+    plt.figure(figsize=(15,15))
+    pos = nx.spring_layout(G, seed=42, k=0.15)
+    nx.draw(G, pos, font_family='NanumGothic', with_labels=True, node_size=node_size, node_color=node_colors, alpha=0.8, linewidths=1,
+            font_size=9, font_color="black", font_weight="medium", edge_color="grey", width=edge_weights)
 
 
-net = Network(notebook=True, cdn_resources='in_line')
+    # 중심 노드들끼리 겹치는 단어 출력
+    overlapping_키워드 = set()
+    for i, keyword1 in enumerate(키워드):
+        for j, keyword2 in enumerate(키워드):
+            if i < j and keyword1 in G and keyword2 in G:
+                if nx.has_path(G, keyword1, keyword2):
+                    overlapping_키워드.add(keyword1)
+                    overlapping_키워드.add(keyword2)
+    if overlapping_키워드:
+        print(f"다음 중심 키워드들끼리 연관성이 있어 중복될 가능성이 있습니다: {', '.join(overlapping_키워드)}")
 
-net.from_nx(G)
 
-net.save_graph(f'/app/streamlit/pyvis_graph.html')
-HtmlFile = open(f'/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
+    net = Network(notebook=True, cdn_resources='in_line')
+
+    net.from_nx(G)
+
+    return net
+
+
 
 with col2_2:
-    components.html(HtmlFile.read(), height=435)
+    try:
+        net = 네트워크(review)
+        net.save_graph(f'/app/streamlit/pyvis_graph.html')
+        HtmlFile = open(f'/app/streamlit/pyvis_graph.html', 'r', encoding='utf-8')
+        components.html(HtmlFile.read(), height=435)
+    except:
+        st.write('존재하지 않는 키워드예요.')
 
 ########################################################################################################################
 ########################################################################################################################
