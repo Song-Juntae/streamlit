70896674573b7f3c36bfe4ad93b6bacf2a253489
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-05 17:17:24 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -108,6 +108,7 @@ with col1_4:
         max_value=마지막날짜
     )
     type(end_date)
+    end_date
     기간마스크 = ((df_리뷰_감성분석결과['time'] >= pd.Timestamp(start_date[0]) & df_리뷰_감성분석결과['time'] <= pd.Timestamp(end_date)))
 
 with col3_1:
