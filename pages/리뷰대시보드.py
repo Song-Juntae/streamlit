3192d3d399d509961a8e27506e7061f3543e306e
gimmy <123876627+zjan0ghy@users.noreply.github.com>
2023-05-05 23:23:50 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -148,7 +148,8 @@ with col1_4:
         # st.write('문제가 생겼어요.')
      
 ########################################################################################################################
-def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=None, sentiment = None, item = None, source = None, 품사='noun'):
+def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=None, 
+                        sentiment = None, item = None, source = None, 품사='noun'):
     if name is not None:
         df = df[df['name'] == name]
     if sentiment is not None:
@@ -168,7 +169,8 @@ def get_count_top_words(df, start_date=None, last_date=None, num_words=10, name=
     count_top_words = count_df.sum().sort_values(ascending=False).head(num_words).to_dict()
     return count_top_words
 
-def get_tfidf_top_words(df, start_date=None, last_date=None, num_words=10, name=None, sentiment = None, item = None, source = None, 품사='noun'):
+def get_tfidf_top_words(df, start_date=None, last_date=None, num_words=10, name=None, 
+                        sentiment = None, item = None, source = None, 품사='noun'):
     if name is not None:
         df = df[df['name'] == name]
     if sentiment is not None:
