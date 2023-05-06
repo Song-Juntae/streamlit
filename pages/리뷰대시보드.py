1181718f59ca0bc233951acb808947ef0f8f92a3
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-07 01:45:04 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -218,7 +218,10 @@ if option == '중요도(TF-IDF)':
 # 파이차트
 with col4_1:
     마스크된데이터프레임
-    df_파이차트 = pd.DataFrame(마스크된데이터프레임['sentiment'].value_counts())
+    if len(키워드) > 1:
+        df_파이차트 = pd.DataFrame(마스크된데이터프레임['sentiment'][마스크된데이터프레임['replace_slang_sentence'].str.contains('|'.join(키워드))].value_counts())
+    if len(키워드) == 1:
+        df_파이차트 = pd.DataFrame(마스크된데이터프레임['sentiment'][마스크된데이터프레임['replace_slang_sentence'].str.contains(키워드)].value_counts())
     pie_chart = go.Figure(data=[go.Pie(labels=list(df_파이차트.index), values=df_파이차트['count'])])
     st.plotly_chart(pie_chart, use_container_width=True)
 with col4_2:
