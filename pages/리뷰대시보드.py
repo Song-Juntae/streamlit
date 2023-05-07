ac31358d9d7652e1dd1df75cd49957bbd0c51502
gimmy <123876627+zjan0ghy@users.noreply.github.com>
2023-05-08 02:22:50 +0900

    0

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -31,7 +31,7 @@ from pyvis.network import Network
 from wordcloud import WordCloud
 ########################################################################################################################
 # 데이터 로드 상수
-df_리뷰_감성분석결과 = pd.read_csv('/app/streamlit/data/리뷰6차.csv')
+df_리뷰_감성분석결과 = pd.read_csv('/app/streamlit/data/리뷰9차.csv')
 df_리뷰_감성분석결과['time'] = pd.to_datetime(df_리뷰_감성분석결과['time'])
 
 stopwords = ['언늘', '결국', '생각', '후기', '감사', '진짜', '완전', '사용', '요즘', '정도', '이번', '달리뷰', '결과', 
